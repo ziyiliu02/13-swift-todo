a0b341fcae2b8538215d9e406eab315f344d3fb2
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-21 16:26:33 +0800

    Initial Commit

---
+++ 13-swift-todo/_3_swift_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_swift_todoApp.swift
+//  13-swift-todo
+//
+//  Created by Liu Ziyi on 21/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _3_swift_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
