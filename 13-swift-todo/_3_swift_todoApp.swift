b89484a9c41547821a324c3c01eba9b213f8bc80
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-21 21:59:42 +0800

    - setup firebase

--- 13-swift-todo/_3_swift_todoApp.swift
+++ 13-swift-todo/_3_swift_todoApp.swift
@@ -5,10 +5,15 @@
 //  Created by Liu Ziyi on 21/7/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct _3_swift_todoApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
