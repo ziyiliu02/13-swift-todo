21dc1fd71b904905ac5ce9e5aadb29183a6821d7
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-22 23:24:24 +0800

    - build out the login view

---
+++ 13-swift-todo/Views/HeaderView.swift
@@ -0,0 +1,41 @@
+//
+//  HeaderView.swift
+//  13-swift-todo
+//
+//  Created by Liu Ziyi on 22/7/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(Color.pink)
+                .rotationEffect(Angle(degrees: 15))
+            
+            VStack {
+                Text("To Do List")
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
+                    .bold()
+                
+                Text("Get things done")
+                    .font(.system(size: 30))
+                    .foregroundColor(Color.white)
+            }
+            .padding(.top, 30)
+        }
+        .frame(width: UIScreen.main.bounds.width * 3,
+               height: 300)
+        .offset(y: -100)
+        
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
