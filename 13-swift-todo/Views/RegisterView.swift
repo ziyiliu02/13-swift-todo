0f61ea522fe7f69d7ff507c8b0624ddf5a0f6cfd
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-24 17:25:13 +0800

    - work on signing up new users -- registration. - create a new user model to represent a user. - work with firebase auth and Firestone to both create and save user records in the database.

--- 13-swift-todo/Views/RegisterView.swift
+++ 13-swift-todo/Views/RegisterView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct RegisterView: View {
     
-    @State var name = ""
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = RegisterViewViewModel()
     
     var body: some View {
         
@@ -23,20 +21,21 @@ struct RegisterView: View {
                        background: .orange)
             
             Form {
-                TextField("Full Name", text: $name)
+                TextField("Full Name", text: $viewModel.name)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
                 
-                TextField("Email Address", text: $email)
+                TextField("Email Address", text: $viewModel.email)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .autocapitalization(.none)
                     .autocorrectionDisabled()
                 
-                SecureField("Password", text: $password)
+                SecureField("Password", text: $viewModel.password)
                     .textFieldStyle(DefaultTextFieldStyle())
                 
                 TLButton(title: "Create Account", background: .green) {
-                    // Attempt registration 
+                    // Attempt registration
+                    viewModel.register()
                 }
                 .padding()
 
