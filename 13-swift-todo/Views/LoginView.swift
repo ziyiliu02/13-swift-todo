0de7bbf07729a77fb8287e711939bfc0c5b84c25
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-24 14:10:03 +0800

    - continue building the register view - create a reusable button - move the business logic from the login view to the login view model

--- 13-swift-todo/Views/LoginView.swift
+++ 13-swift-todo/Views/LoginView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = LoginViewViewModel()
     
     var body: some View {
         
@@ -25,24 +24,17 @@ struct LoginView: View {
                 
                 // Login Form
                 Form {
-                    TextField("Email Address", text: $email)
+                    TextField("Email Address", text: $viewModel.email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                    
+                    SecureField("Password", text: $viewModel.password)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
-                    Button {
-                        // Attempt login
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color.blue)
-                            
-                            Text("Log In")
-                                .foregroundColor(Color.white)
-                                .bold()
-                        }
+                    TLButton(title: "Log In", background: .blue) {
+                        // Attempt log in
                     }
+                    .padding()
 
                 }
                 .offset(y: -50)
--- 13-swift-todo/Views/RegisterView.swift
+++ 13-swift-todo/Views/RegisterView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         
         VStack {
@@ -16,6 +21,27 @@ struct RegisterView: View {
                        subtitle: "Start organising todos",
                        angle: -15,
                        background: .orange)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account", background: .green) {
+                    // Attempt registration 
+                }
+                .padding()
+
+            }
+            .offset(y: -50)
             
             Spacer()
         }
