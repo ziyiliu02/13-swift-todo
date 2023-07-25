8e6e276495c41e585985cf11435864fce30a07b2
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-25 16:57:46 +0800

    - implement Done Checkmarks

--- 13-swift-todo/ViewModels/ToDoListItemViewViewModel.swift
+++ 13-swift-todo/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Liu Ziyi on 22/7/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 // ViewModel for a single to do list item view (each row in items list)
@@ -12,6 +14,18 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
