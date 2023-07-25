f3d6940823e9d7aff3ac6b5c5f631c06bc873e0d
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-25 14:08:55 +0800

    - create the New To Do List Item View.

--- 13-swift-todo/ViewModels/ToDoListViewViewModel.swift
+++ 13-swift-todo/ViewModels/ToDoListViewViewModel.swift
@@ -11,5 +11,7 @@ import Foundation
 // ViewModel for list of items view
 // Primary tab 
 class ToDoListViewViewModel: ObservableObject {
+    @Published var showingNewItemView = false 
+    
     init() {}
 }
